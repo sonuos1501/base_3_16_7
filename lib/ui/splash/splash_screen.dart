9978d824e0266f7c104dc95e1048bd60caede7e2
sonuos1501 <son.uospt@gmail.com>
2024-01-19 11:28:14 +0700

    add app name and package name

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:base_3_16_7/utils/custom_log/custom_log.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:package_info/package_info.dart';
 import '../../blocs/common_blocs/authentication/authentication_bloc.dart';
 import '../../gen/assets.gen.dart';
 
@@ -11,6 +13,20 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      final packageInfo = await PackageInfo.fromPlatform();
+
+      final appName = packageInfo.appName;
+      final packageName = packageInfo.packageName;
+
+      logSuccess('APP Name: $appName');
+      logSuccess('Package Name: $packageName');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AuthenticationBloc, AuthenticationState>(
