86c6baca2f86a68aa0b1ec9bd8347fa88d96caff
sonuos1501 <son.uospt@gmail.com>
2024-01-24 14:47:30 +0700

    refactor string

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -4,21 +4,6 @@ class Strings {
   //General
   static const String appName = 'Show Player';
 
-  static const contactMailAddress = 'support@gmoretech.com';
-
-  static const contactPhoneNumber = '0364165500';
-  static const personalId = 'QWNjb3VudEtpbmQtMTA=';
-  static const companyId = 'QWNjb3VudEtpbmQtMjA=';
-  static const keyHash = '59e3e2708cdd699bfc9d3e8029350b4704c43591b52992f428190ee069cebcc1';
-  static const packageNameOwnerApp = 'com.chintaidx.kanri.owner';
-  static const packageNamePartnerApp = 'com.chintaidx.kanri.partner';
-  static const packageNameResidentApp = 'com.chintaidx.kanri.resident';
-  static const packageNameKanriApp = 'com.chintaidx.kanri.admin';
-  static const ownerApp = 'OwnerApp';
-  static const partnerApp = 'PartnerApp';
-  static const residentApp = 'ResidentApp';
-  static const kanriApp = 'KanriApp';
-
   static const empty = '';
 
 }
